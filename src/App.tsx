727a010d891797bd919d80909305b79fca03305f
Arthur Bertoti <artgbertoti@gmail.com>
2024-08-17 14:50:52 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import { Home } from "./pages/index"
+
+export function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  )
+}
